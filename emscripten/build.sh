3f151b3ad4c03398f71ddd5ea8c932aefdf5d4af
Vadim Kantorov <vadimkantorov@gmail.com>
2017-01-29 03:37:48 +0100

    git clone prototype

--- emscripten/build.sh
+++ emscripten/build.sh
@@ -1,7 +1,9 @@
 set -e
+
 cd libgit2
-rm -rf build
+[ -d build ] && rm -rf build
 mkdir -p build && cd build
+
 cmake .. -DCMAKE_SIZEOF_VOID_P=8 -DCMAKE_C_COMPILER="${EMSCRIPTEN_ROOT_PATH}/emcc" -DCMAKE_AR="${EMSCRIPTEN_ROOT_PATH}/emar" -DCMAKE_RANLIB="${EMSCRIPTEN_ROOT_PATH}/emranlib" -DCMAKE_C_FLAGS=-O2
 cmake --build .
-"${EMSCRIPTEN_ROOT_PATH}/emcc" -s LINKABLE=1 -O2 -o libgit.js libgit2.so
+"${EMSCRIPTEN_ROOT_PATH}/emcc" -s LINKABLE=1 -O2 -o emscripten/libgit2.js libgit2.so
--- gittex.js
+++ gittex.js
@@ -1,4 +1,11 @@
+git_clone = Module.cwrap('git_clone', 'number', ['number', 'string', 'string', 'number'])
+
 function gittex_eval(command)
 {
+	var repo = Module._malloc(new Uint32Array(1).BYTES_PER_ELEMENT);
+	//Module.HEAPU8.set(myTypedArray, repo);
+	var ret = git_clone(repo, 'http://github.com/vadimkantorov/gittex', 'gittex');
+	Module._free(repo)
+	console.log(ret);
 	return "Command was: " + command;
 }
