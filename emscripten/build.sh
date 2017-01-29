fa7609474b3e29dd130efa78f73c55ff952e9f27
Vadim Kantorov <vadimkantorov@gmail.com>
2017-01-29 01:26:43 +0100

    build script fix

--- emscripten/build.sh
+++ emscripten/build.sh
@@ -4,4 +4,4 @@ rm -rf build
 mkdir -p build && cd build
 cmake .. -DCMAKE_SIZEOF_VOID_P=8 -DCMAKE_C_COMPILER="${EMSCRIPTEN_ROOT_PATH}/emcc" -DCMAKE_AR="${EMSCRIPTEN_ROOT_PATH}/emar" -DCMAKE_RANLIB="${EMSCRIPTEN_ROOT_PATH}/emranlib" -DCMAKE_C_FLAGS=-O2
 cmake --build .
-"${EMSCRIPTEN_ROOT_PATH}/emcc" -s LINKABLE=1 -o libgit.js libgit2.so
+"${EMSCRIPTEN_ROOT_PATH}/emcc" -s LINKABLE=1 -O2 -o libgit.js libgit2.so
