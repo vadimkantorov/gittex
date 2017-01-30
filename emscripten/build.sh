ca72a71983a0480c59ea51f30ffe5ed8667c222d
Vadim Kantorov <vadimkantorov@gmail.com>
2017-01-30 18:07:47 +0100

    fixed build.sh

--- emscripten/build.sh
+++ emscripten/build.sh
@@ -1,9 +1,36 @@
-set -e
+[ -d libgit2 ] || git clone https://github.com/libgit2/libgit2
 
 cd libgit2
 [ -d build ] && rm -rf build
 mkdir -p build && cd build
 
-cmake .. -DCMAKE_SIZEOF_VOID_P=4 -DCMAKE_C_COMPILER="${EMSCRIPTEN_ROOT_PATH}/emcc" -DCMAKE_AR="${EMSCRIPTEN_ROOT_PATH}/emar" -DCMAKE_RANLIB="${EMSCRIPTEN_ROOT_PATH}/emranlib" -DCMAKE_C_FLAGS=$'-s EXPORTED_FUNCTIONS="[\'_git_clone\', \'_git_libgit2_init\']" -O2'
-MAKEFLAGS=8 cmake --build .
-"${EMSCRIPTEN_ROOT_PATH}/emcc" -s EXPORTED_FUNCTIONS="['_git_clone','_git_libgit2_init']" -O2 -s LINKABLE=1 -o ../../libgit2.js libgit2.so
+cat <<EOF > mytest.c
+	#include <git2.h>
+
+	int main()
+	{
+		git_repository* repo = NULL;
+		git_libgit2_init();
+		if(0 != git_clone(&repo, "git://git.kernel.org/pub/scm/utils/dash/dash.git", "dash", NULL))
+			printf("Error: %s\n", giterr_last()->message);
+	}
+EOF
+
+function build_test_emscripten
+{
+	cmake .. -DCMAKE_SIZEOF_VOID_P=4 -DCMAKE_C_COMPILER="${EMSCRIPTEN_ROOT_PATH}/emcc" -DCMAKE_AR="${EMSCRIPTEN_ROOT_PATH}/emar" -DCMAKE_RANLIB="${EMSCRIPTEN_ROOT_PATH}/emranlib" -DCMAKE_C_FLAGS=$'-s EXPORTED_FUNCTIONS="[\'_git_clone\', \'_git_libgit2_init\',\'giterr_last\']" -O2' -DBUILD_CLAR=OFF -DUSE_SSH=OFF -DCURL=OFF -DUSE_OPENSSL=OFF
+	MAKEFLAGS=8 cmake --build .
+	"${EMSCRIPTEN_ROOT_PATH}/emcc" -s EXPORTED_FUNCTIONS="['_git_clone','_git_libgit2_init','_giterr_last']" -O2 -s LINKABLE=1 -o ../../libgit2.js libgit2.so
+	"${EMSCRIPTEN_ROOT_PATH}/emcc" -o mytest.js mytest.c -I../include libgit2.so
+	nodejs ./mytest.js
+}
+
+function build_test_native
+{
+	cmake .. -DCMAKE_C_FLAGS=$'-O2' -DBUILD_CLAR=OFF -DUSE_SSH=OFF -DCURL=OFF -DUSE_OPENSSL=OFF
+	MAKEFLAGS=8 cmake --build .
+	gcc -o mytest mytest.c -I../include -lgit2
+	./mytest
+}
+
+build_test_emscripten
