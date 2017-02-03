6f48bf9898a35d6d27624434f6cb7d3c7e3f841b
Vadim Kantorov <vadimkantorov@gmail.com>
2017-02-03 03:10:58 +0100

    Update gittex.js

--- gittex.js
+++ gittex.js
@@ -6,21 +6,23 @@ NULL = 0;
 function github_api_transport_cb(out, owner, param) //git_transport_cb
 {
 	out = {
-		version : 1,
-		connected : 1,
-		set_custom_headers : function(transport, custom_headers) { },
-		connect : function(transport, url, cred_acquire_cb, cred_acquire_payload, proxy_opts, direction, flags) { transport.connected = 1; return 0; },
+		connect : function(transport, url, cred_acquire_cb, cred_acquire_payload, proxy_opts, direction, flags) { transport.connected = 1; transport.flags = flags; return 0; },
 		ls : function(out, size, transport) { },
 		push : function(transport, push, callbacks) { },
 		negotiate_fetch : function(transport, repo, refs, count) { },
 		download_pack : function(transport, repo, stats, progress_cb, progress_payload) { },
-		read_flags : function(transport, flags) { },
+		
+		set_custom_headers : function(transport, custom_headers) { console.log('transport.set_custom_headers'); transport.custom_headers = custom_headers; return 0; }, // convert from git_strarray* to UTF16
+		read_flags : function(transport, flags) { console.log('transport.read_flags'); flags = transport.flags; return 0; }, // write to int* flags the value
 		
 		set_callbacks : function(transport, progress_cb, error_cb, certificate_check_cb, payload) { console.log('transport.set_callbacks', 'nop'); return 0; },
 		is_connected : function(transport) { console.log('transport.is_connected'); return transport.connected; },
 		cancel : function(transport) { console.log('transport.cancel', 'nop'); return 0; },
 		close : function(transport) { console.log('transport.close'); transport.connected = 0; return 0; },
 		free : function(transport) { console.log('transport.free', 'nop'); return 0; }
+		
+		version : 1,
+		connected : 0,
 	}
 }
 
