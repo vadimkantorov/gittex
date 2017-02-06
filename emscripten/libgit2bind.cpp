abf517f686cfc3b375c17db16cc7afbb4b7e5b85
Vadim Kantorov <vadimkantorov@gmail.com>
2017-02-06 17:31:24 +0100

    embind work started

--- emscripten/libgit2bind.cpp
+++ emscripten/libgit2bind.cpp
@@ -1,14 +1,15 @@
 #include <git2.h>
+#include <git2/sys/transport.h>
 #include <emscripten/bind.h>
 
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(libgit2)
 {
-  function("git_clone", &git_clone);
-  function("git_libgit2_init", &git_libgit2_init);
-  function("giterr_last", &giterr_last);
-  function("git_transport_register", &git_transport_register);
+  function("git_clone", &git_clone, allow_raw_pointers());
+  function("git_libgit2_init", &git_libgit2_init, allow_raw_pointers());
+  function("giterr_last", &giterr_last, allow_raw_pointers());
+  function("git_transport_register", &git_transport_register, allow_raw_pointers());
   
   //value_object<git_transport>("git_transport");
 }
