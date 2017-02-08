d88a00e16bd65f07a3c5df38e9c16c93af0f8943
Vadim Kantorov <vadimkantorov@gmail.com>
2017-02-08 20:45:10 +0100

    Update gittex.js

--- gittex.js
+++ gittex.js
@@ -12,7 +12,7 @@ var github_git_transport = {
 	ls			: Runtime.addFunction(function(out, size, transport) { console.log('transport.ls', 'nop'); return 0; }),
 	negotiate_fetch		: Runtime.addFunction(function(transport, repo, refs, count) { console.log('transport.negotiate_fetch', 'nop'); return 0;  }),
 	download_pack		: Runtime.addFunction(function(transport, repo, stats, progress_cb, progress_payload) { console.log('transport.download_pack', 'nop'); return 0; }),
-	connect			: Runtime.addFunction(function(transport, url, cred_acquire_cb, cred_acquire_payload, proxy_opts, direction, flags) { console.log('transport.connect', 'nop'); transport.connected = 1; transport.flags = flags; transport.url = Module.UTF8ToString(url); return 0; }),
+	connect			: Runtime.addFunction(function(transport, url, cred_acquire_cb, cred_acquire_payload, proxy_opts, direction, flags) { console.log('transport.connect', 'nop'); github_git_transport.connected = 1; github_git_transport.flags = flags; github_git_transport.url = Module.UTF8ToString(url); return 0; }),
 	read_flags		: Runtime.addFunction(function(transport, flags) { console.log('transport.read_flags'); flags = github_git_transport.flags; return 0; }), // write to int* flags the value
 	is_connected		: Runtime.addFunction(function(transport) { console.log('transport.is_connected'); return github_git_transport.connected; }),
 	push			: Runtime.addFunction(function(transport, push, callbacks) { console.log('transport.push', 'nop'); return 1; }),
@@ -25,24 +25,23 @@ var github_git_transport = {
 	connected : 0,
 };
 
-function struct_pack_i32(array)
+function git_transport_cb(out, owner, param)
 {
-	var unsafe_memory = Module._malloc(Runtime.getNativeFieldSize('i32') * array.length);
-	//Module.HEAP32.set(array, unsafe_memory);
-	for(var i = 0; i < array.length; i++)
-		Module.setValue(unsafe_memory + i * Runtime.getNativeFieldSize('i32'), array[i], 'i32');
-	return unsafe_memory;
-}
-
-function github_transport_cb(out, owner, param) //git_transport_cb
-{
+	var struct_pack_i32 = function(array)
+	{
+		var unsafe_memory = Module._malloc(Runtime.getNativeFieldSize('i32') * array.length);
+		//Module.HEAP32.set(array, unsafe_memory);
+		for(var i = 0; i < array.length; i++)
+			Module.setValue(unsafe_memory + i * Runtime.getNativeFieldSize('i32'), array[i], 'i32');
+		return unsafe_memory;
+	};
 	Module.setValue(out, struct_pack_i32([github_git_transport.version, github_git_transport.set_callbacks, github_git_transport.set_custom_headers, github_git_transport.connect, github_git_transport.ls, github_git_transport.push, github_git_transport.negotiate_fetch, github_git_transport.download_pack, github_git_transport.is_connected, github_git_transport.read_flags, github_git_transport.cancel, github_git_transport.close, github_git_transport.free]), 'i32');
 }
 
 function gittex_test()
 {
 	console.log('init: ', git_libgit2_init());
-	console.log('register: ', git_transport_register('github://', Runtime.addFunction(github_transport_cb), NULL));
+	console.log('register: ', git_transport_register('github://', Runtime.addFunction(git_transport_cb), NULL));
 	console.log('clone:', git_clone(Module._malloc(4), 'github://github.com/vadimkantorov/gittex.git', 'gittex', 0));
 }
 
