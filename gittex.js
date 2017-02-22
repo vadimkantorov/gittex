90712533b6ac5466804b620a264fe1ee4c1d9b9d
Vadim Kantorov <vadimkantorov@gmail.com>
2017-02-22 17:04:51 +0900

    Update gittex.js

--- gittex.js
+++ gittex.js
@@ -77,7 +77,7 @@ var github_git_transport = {
 		git_repository_odb__weakptr(odb, repo);
 		github_revwalk(github_git_transport.url.replace('github://', ''), function(blob_contents, object_type) {
 			var data = Module._malloc(blob_contents.length);
-			Module.writeArrayToMemory(blob, data);
+			Module.writeArrayToMemory(blob_contents, data);
 			git_odb_write(oid, odb, data, blob_contents.length, object_type == "commit" ? git_otype.GIT_OBJ_COMMIT : object_type == "tree" ? git_otype.GIT_OBJ_TREE : object_type == "blob" ? git_otype.GIT_OBJ_BLOB : git_otype.GIT_OBJ_ANY);
 			Module._free(data);
 		});
