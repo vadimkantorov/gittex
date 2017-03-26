bbe01fc0f45a1d9dac9b2fbc5ff2959896638634
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-27 05:03:33 +0900

    Update gittex.js

--- gittex.js
+++ gittex.js
@@ -162,7 +162,7 @@ var github_transport = {
 	github_git_data : function(github_repo_url, object_type, object_id)
 	{
 		var result = null;
-		var api_url = github_repo_url.replace('github://', 'https://').replace('github.com', 'api.github.com/repos') + '/git/' + object_type + 's/' + object_id;
+		var api_url = github_repo_url.replace('github://', 'https://').replace('github.com', 'api.github.com/repos') + '/git/' + object_type + 's/' + object_id + (window.location.hash ? '?access_token=' + window.location.hash : '');
 		$.ajax(api_url, {async : false}).done(function(data) {result = data;});
 		return result;
 	},
